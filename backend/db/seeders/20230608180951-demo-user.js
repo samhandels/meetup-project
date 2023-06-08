6308b9945e45d409490cf8ed4ea8ed199e4b55a9
Sam Handelsman <samhandelsman@gmail.com>
2023-06-08 15:17:21 -0700

    changed db

--- backend/db/seeders/20230608180951-demo-user.js
+++ backend/db/seeders/20230608180951-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: 'Some',
+        lastName: 'Guy',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Just',
+        lastName: 'AnotherGuy',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Third',
+        lastName: 'Person',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
