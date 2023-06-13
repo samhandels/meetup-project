d7cfd268273718110ff15042d37e37c7c52ef71a
Sam Handelsman <samhandelsman@gmail.com>
2023-06-13 10:23:48 -0700

    fixed issue with render

--- backend/db/seeders/20230612202652-demo-group.js
+++ backend/db/seeders/20230612202652-demo-group.js
@@ -30,7 +30,7 @@ module.exports = {
         state: 'CA'
       },
       {
-        organizerId: 4,
+        organizerId: 2,
         name: 'Skateboarding Contest',
         about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         type: 'In person',
@@ -39,7 +39,7 @@ module.exports = {
         state: 'NV'
       },
       {
-        organizerId: 5,
+        organizerId: 3,
         name: 'Star Wars Watch Party',
         about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         type: 'Online',
