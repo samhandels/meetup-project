3df00d46905faae9ff34ef6e392ef9f6d80c1792
Sam Handelsman <samhandelsman@gmail.com>
2023-07-01 15:57:33 -0700

    completed parts of header

---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {useState, useRef} from 'react'
+import { useSelector } from 'react-redux';
+import './LandingPage.css';
+
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,23 +2,57 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import './Navigation.css';
+import favicon from '../../assets/apple-touch-icon.png'
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
 
-  return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
+function Navigation({ isLoaded }) {
+    const sessionUser = useSelector((state) => state.session.user);
+
+    let sessionLinks;
+    if (sessionUser) {
+      sessionLinks = (
+        <>
+        <li>
+          <NavLink exact to="/start-group">Start a new group</NavLink>
+        </li>
         <li>
           <ProfileButton user={sessionUser} />
         </li>
-      )}
-    </ul>
-  );
-}
+      </>
+    );
+    } else {
+      sessionLinks = (
+        <li>
+          <OpenModalButton
+            buttonText="Log In"
+            modalComponent={<LoginFormModal />}
+            className="login"
+          />
+          <OpenModalButton
+            buttonText="Sign Up"
+            modalComponent={<SignupFormModal />}
+            className="signup"
+          />
+        </li>
+      );
+    }
 
-export default Navigation;
+    return (
+      <nav>
+        <div className="logo-container">
+        <NavLink exact to="/">
+          <img src={favicon} alt="Favicon" className="favicon" /> {/* Use the updated favicon import */}
+        </NavLink>
+        </div>
+        <ul className="navigation-links">
+        {isLoaded && sessionLinks}
+        </ul>
+      </nav>
+    );
+  }
+
+  export default Navigation;
