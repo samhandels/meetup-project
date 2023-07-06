7349c2e1d0343f7841daf0021eab24b18a4a630f
Sam Handelsman <samhandelsman@gmail.com>
2023-07-06 16:40:15 -0700

    starting group create

---
+++ frontend/src/components/GroupCreate/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import * as groupActions from "../../store/groups";
+import "./CrtGrpFrm.css";
+
+const GroupCreate = () => {
+
+}
+
+export default GroupCreate;
