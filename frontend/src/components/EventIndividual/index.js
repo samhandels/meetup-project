e8a27b517d36e0d2ca983dfe445ff6fd35419028
Sam Handelsman <samhandelsman@gmail.com>
2023-07-06 16:11:24 -0700

    worked on events individual

--- frontend/src/components/EventIndividual/index.js
+++ frontend/src/components/EventIndividual/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { thunkGetIndividualEvent } from "../../store/events";
+import './EventIndividual.css';
 
 export const EventIndividual = () => {
   const dispatch = useDispatch();
@@ -16,19 +17,25 @@ export const EventIndividual = () => {
   }, [dispatch]);
 
 
+  if (!eventInfo[eventId] || !event.EventImages || event.EventImages.length === 0) {
+    return null;
+  }
+
+
+
   const sendToGroup = () => {
     history.push(`/groups/${event.Group.id}`);
   };
 
   const imageCheck = () => {
-    // if (event.previewImage.length <= 0) {
-    //   return "https://vishwaentertainers.com/wp-content/uploads/2020/04/No-Preview-Available.jpg";
-    // } else {
-    //   return `${event.previewImage[0].url}`;
-    // }
+    if (event.previewImage === "no preview image" || event.previewImage === undefined) {
+        event.previewImage = "https://images.unsplash.com/photo-1623018035782-b269248df916?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80";
+      } else {
+      return `${event.EventImages[0].url}`;
+    }
   };
 
-  const eventPriceCheck = () => {
+  const eventPrice = () => {
     if (event.price <= 0) {
       return "FREE";
     } else {
@@ -41,16 +48,7 @@ export const EventIndividual = () => {
       <div className="event-detail-container">
         <div className="event-detail-breadcrumb">
           <p>{"<"}</p>
-          <Link
-            style={{
-              fontSize: "12px",
-              fontWeight: "500",
-              color: "#00798A",
-            }}
-            to="/events"
-          >
-            Events
-          </Link>
+          <Link to="/events">Events</Link>
         </div>
         <div className="event-detail-header-container">
           <h2>{event.name}</h2>
@@ -59,19 +57,14 @@ export const EventIndividual = () => {
       </div>
       <div className="event-detail-body-container">
         <div className="event-detail-body-info">
-          <img
-            className="event-detail-body-image"
-            width="450"
-            height="250"
-            src={imageCheck()}
-          />
+          <img className="event-detail-body-image" width="500" height="300" src={imageCheck()} />
           <div className="event-detail-body-info-group">
             <img
               onClick={sendToGroup}
               className="event-detail-body-info-group-image"
-              width="100"
+              width="110"
               height="75"
-            //   src={`${event.previewImage[0].url}`}
+              src={`${event.EventImages[0].url}`}
             />
             <div className="event-detail-body-info-group-body">
               <h4 onClick={sendToGroup}>{event.Group.name}</h4>
@@ -79,7 +72,7 @@ export const EventIndividual = () => {
           </div>
           <div className="event-detail-body-info-event">
             <div className="event-detail-body-info-event-time-details">
-              <i className="far fa-clock fa-lg" style={{ color: "#CCCCCC" }}></i>
+              <i className="far fa-regular fa-clock fa-lg"></i>
               <div className="event-detail-body-info-event-details-time-container">
                 <div className="event-detail-body-info-event-details-start-time">
                   <span>START </span>
@@ -98,27 +91,23 @@ export const EventIndividual = () => {
               </div>
             </div>
             <div className="event-detail-body-info-event-price-details">
-              <i className="fa-solid fa-sack-dollar fa-xl" style={{ color: "#CCCCCC" }}></i>
-              <p>{eventPriceCheck()}</p>
+              <i className="fa-regular fa-sack-dollar"></i>
+              <p>{eventPrice()}</p>
             </div>
             <div className="event-detail-body-info-event-type-details">
-              <i className="fa-solid fa-map-pin fa-xl" style={{ color: "#CCCCCC" }}></i>
+              <i className="fa-solid fa-map-pin fa-xl"></i>
               <p>{event.type}</p>
             </div>
-            <div className="event-detail-body-info-event-button">
-              {/* <EventDetailButton event={event} /> */}
-            </div>
+            <div className="event-detail-body-info-event-button">{/* <EventDetailButton event={event} /> */}</div>
           </div>
         </div>
         <div className="event-detail-body-description">
-          <h2 style={{ marginBottom: ".25rem" }}>Details</h2>
+          <h2>Details</h2>
           <p>{event.description}</p>
         </div>
       </div>
     </>
   );
-
-
 };
 
 export default EventIndividual;
